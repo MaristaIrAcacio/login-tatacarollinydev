f5f00b16512a19664df28705f4dfc600377a6aee
Thaynna Carolliny <tatacarolliny24@gmail.com>
2023-07-05 10:14:13 -0300

    geracao de lista

--- login.js
+++ login.js
@@ -7,6 +7,28 @@ function Verificar(){
     if(!user || !senha){
         alert("Campos de preenchimento obrigatorio. Favor preencher!");
     } else{
-        alert("Campos preenchidos com sucesso!");
+        window.location.href = "aula2.html";
+    }
+
+}
+
+var dadosLista = [];
+
+function salvarUser() {
+    let nomeUser = document.getElementById("nomeUser").value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+    
+        criaLista()
     }
 }
+
+function criaLista(){
+    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome usuario</th><th>Ações</th></tr>";
+
+    for(let i = 0; i <= (dadosLista.length-1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success'onclick=''>Editar</button><button class='btn btn-danger'onclick=''>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    };
+}; 
