f28e102ab90277a2100030bf4384b9d29ab1872c
Thaynna Carolliny <tatacarolliny24@gmail.com>
2023-07-26 09:30:41 -0300

    implantacao das funcoes "editar e excluir"

--- login.js
+++ login.js
@@ -28,7 +28,17 @@ function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome usuario</th><th>Ações</th></tr>";
 
     for(let i = 0; i <= (dadosLista.length-1); i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success'onclick=''>Editar</button><button class='btn btn-danger'onclick=''>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success'onclick='editar (this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger'onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     };
 }; 
+
+function excluir(i) {
+    dadosLista.splice((i-1), 1);
+    document.getElementById("tabela").deleteRow(i);
+};
+
+function editar(i) {
+    document.getElementById("nomeUser").value = dadosLista[(i-1)];
+    dadosLista.splice(dadosLista[(i-1),1]);
+};
